c8e1f476f4324faed5c7652a5b67015ce4a8a7c9
chris / brookins <chris@brookins.info>
2019-07-03 08:25:57 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@
 #
 #
 ###
-echo -e "${Yellow}If you have not edited ssh.et.service${NC}"
-echo -e "${Yellow}Please press CTRL + C now to exit${NC}"
+echo "${Yellow}If you have not edited ssh.et.service${NC}"
+echo "${Yellow}Please press CTRL + C now to exit${NC}"
 read -p "Press enter to continue"
 
 # User Variables
@@ -23,18 +23,18 @@ NC='\033[0m' # No Color
 
 # Upgrading OS
 clear
-echo -e "${green}Upgrading OS${NC}"
+echo "${green}Upgrading OS${NC}"
 apt-get update && apt-get upgrade -y
 clear
 
 # Setting up reverse SSH
-echo -e "${green}Setting up reverse SSH${NC}"
+echo "${green}Setting up reverse SSH${NC}"
 cp ~/pentest_dropbox/ssh.et.service /lib/systemd/system/
 chmod +x /lib/systemd/system/ssh.et.service
 systemctl enable /lib/systemd/system/ssh.et.service
 
 # Setting up VNC resolution
-echo -e "${green}Setting VNC resolution${NC}"
+echo "${green}Setting VNC resolution${NC}"
 echo "framebuffer_width=1280" | sudo tee -a /boot/config.txt
 echo "framebuffer_width=720" | sudo tee -a /boot/config.txt
 
